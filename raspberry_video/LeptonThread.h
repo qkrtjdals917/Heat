c40432658302171ea08bf9a71dde6427a4e23604
jho9862 <63282383+jho9862@users.noreply.github.com>
2020-06-16 00:13:23 +0900

    Create LeptonThread.h

---
+++ raspberry_video/LeptonThread.h
@@ -0,0 +1,52 @@
+#ifndef TEXTTHREAD
+#define TEXTTHREAD
+
+#include <ctime>
+#include <stdint.h>
+
+#include <QThread>
+#include <QtCore>
+#include <QPixmap>
+#include <QImage>
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MAXTIMINGS 83
+#define DHTPIN 7
+#define PACKET_SIZE 164
+#define PACKET_SIZE_UINT16 (PACKET_SIZE/2)
+#define PACKETS_PER_FRAME 60
+#define FRAME_SIZE_UINT16 (PACKET_SIZE_UINT16*PACKETS_PER_FRAME)
+#define FPS 27;
+
+extern uint16_t frameBuffer1[PACKET_SIZE_UINT16*PACKETS_PER_FRAME];
+
+class LeptonThread : public QThread
+{
+  Q_OBJECT;
+
+public:
+  LeptonThread();
+  ~LeptonThread();
+
+  void run();
+
+public slots:
+  void performFFC();
+
+signals:
+  void updateText(QString);
+  void updateImage(QImage);
+
+private:
+
+  QImage myImage;
+
+  uint8_t result[PACKET_SIZE*PACKETS_PER_FRAME];
+  uint16_t *frameBuffer;
+
+};
+
+#endif
